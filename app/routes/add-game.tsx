5e996a6fe0cca68810af2fc1e333ab55a0f29389
HI-Jamie-Munro-HI <1841793@hc.ac.je>
2025-04-24 11:47:17 +0100

    Created AddGame basic page with title

---
+++ app/routes/add-game.tsx
@@ -0,0 +1,7 @@
+export default function AddGame() {
+  return (
+    <div className="container mx-auto py-20">
+      <h1 className="font-bold text-6xl text-center">Add Game</h1>
+    </div>
+  );
+}
